df8584e827510c560e50313e751c420668d88422
JJRTechSec <jjrtechsec@gmail.com>
2026-03-12 22:57:34 +0100

    - Added icons to html - Hamburger button functionality - Header styling fixed for small screens

---
+++ chamber/scripts/chamber.js
@@ -0,0 +1,10 @@
+const navButton = document.querySelector('#navButton');
+const navBar = document.querySelector('.navigation');
+
+const cards = document.querySelector('cards');
+const url = './data/members.json';
+
+navButton.addEventListener('click', () => {
+  navButton.classList.toggle('show');
+  navBar.classList.toggle('show');
+});
