e384a9c8c2cbedff189c48445a4e33e365bf8988
JJRTechSec <jjrtechsec@gmail.com>
2026-03-12 23:37:01 +0100

    - Added functions to chamber.js and edited to fetch the business data and match info needed - Still need to correct and add a few more things, but the object is successfully retrieved - Added linkedin icon to footer - Edited styling for layout-button

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -8,3 +8,42 @@ navButton.addEventListener('click', () => {
   navButton.classList.toggle('show');
   navBar.classList.toggle('show');
 });
+
+async function getBusinessData() {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.table(data.newBusiness);
+  displayBusinesses(data.newBusiness);
+};
+
+const displayBusinesses = (businesses) => {
+  businesses.forEach((business) => {
+    let card = document.createElement('section');
+    let businessName = document.createElement('h2');
+    let logo = document.createElement('img');
+    let address = document.createElement('span');
+    let phoneNumber = document.createElement('span');
+    let website = document.createElement('span');
+    let email = document.createElement('span');
+
+    businessName.textContent = business.companyName;
+
+    logo.setAttribute('src', business.imageurl);
+    logo.setAttribute('alt', `Logo for ${business.companyName}`);
+    logo.setAttribute('loading', 'lazy');
+    logo.setAttribute('width', '150');
+    logo.setAttribute('height', '200');
+
+    card.classList.add('card');
+
+    card.appendChild(businessName);
+    card.appendChild(address);
+    card.appendChild(phoneNumber);
+    card.appendChild(website);
+    card.appendChild(email);
+    card.appendChild(logo);
+    cards.appendChild(card);
+  });
+};
+
+getBusinessData();
