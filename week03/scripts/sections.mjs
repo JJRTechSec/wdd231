0ba7e6501925b832c7e99b8fae7a7c0223c98d54
JJRTechSec <jjrtechsec@gmail.com>
2026-03-19 14:39:06 +0100

    - Added majority of content to index.html - Styled major parts of index.html - Fixed font-sizing in header

--- week03/scripts/sections.mjs
+++ week03/scripts/sections.mjs
@@ -1,9 +1,16 @@
-export function setSectionSelection(sections) {
+export function setSectionSelection(section) {
   const sectionSelect = document.querySelector("#sectionNumber");
-  sections.forEach((section) => {
+  byuiCourse.sections.forEach((section) => {
     const option = document.createElement("option");
     option.value = section.sectionNumber;
     option.textContent = `${section.sectionNumber}`;
     sectionSelect.appendChild(option);
   });
 }
+
+export function setTitle(course) {
+  document.querySelector("#courseName").textContent = course.name;
+  document.querySelector("#courseCode").textContent = course.code;
+};
+
+export { setTitle } 
